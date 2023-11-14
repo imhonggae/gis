c443691a714b3bdb1c3f0b00c1f0933bda05a911
WinkiH <146861591+WinkiH@users.noreply.github.com>
2023-11-14 23:23:16 +0000

    Cleanup

--- gender_inequality_index.R
+++ gender_inequality_index.R
@@ -14,3 +14,32 @@ diff_data <- gender_data %>%
 merged_data <- dplyr::left_join(world_spatial_data, diff_data, by = c("COUNTRY" = "country"))
 
 #test git pull
+
+# Read Shapefile
+global_index <- st_read(here::here( "World_Countries_Generalized.shp"))
+global_index_edit <- global_index %>%
+  dplyr::select(COUNTRY,
+                ISO,
+                SHAPE_Leng,
+                SHAPE_Area,
+                geometry)
+qtm(global_index_edit)
+
+# Plot up the map
+global_index_plot <- global_index_edit %>%
+  left_join(.,
+            gii_diff_compare,
+            by = c("COUNTRY" = "country"))
+
+tmap_mode("plot")
+qtm(global_index_plot,
+    fill = "Index_Diff201019")
+
+map <- tm_shape(global_index) +
+  tm_borders(lwd = 0.3) +
+  tm_fill("gii_dff", midpoint=0, palette=brewer.pal(6, "RdBu"),
+          title = paste("Change in Gender Inequality Index (2019-2010)"),
+          textNA = "Unknown", style="jenks") +
+  tm_layout(legend.width =0.3,
+            legend.position = c("left","bottom"))
+tmap_save(map, "map.png")
